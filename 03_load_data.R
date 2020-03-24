6bdc3594bb76e1020c183e8d9fdfef4d14a5bf10
DiReese <57525480+DiReese@users.noreply.github.com>
2020-03-24 18:20:37 +0000

    Update 03_load_data.R

--- 03_load_data.R
+++ 03_load_data.R
@@ -1,11 +1,11 @@
 # Write code here to load the data you downloaded in download_data.R
 
-uci.iris <- readRDS("Data/uci_iris.rds")
+uci.bank <- readRDS("Data/uci_bank.rds")
 
 # You might choose to do any resampling here to ensure it is consistent across
 # models
 
-set.seed(7482) # set seed for reproducibility
+#set.seed(7482) # set seed for reproducibility
 
-library("rsample")
-uci.iris.cv <- rsample::vfold_cv(uci.iris, v = 3, strata = Class)
+#library("rsample")
+#uci.bank.rsample <- rsample::vfold_cv(uci.bank, v = 3, strata = y)
